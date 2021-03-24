e0f5d429af82b4dc03141544f561334c49915f1d
AbhiC7721 <abhic7721@gmail.com>
2021-03-25 00:34:31 +0530

    Added profile-frontend

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Abhinaba Chowdhury",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Abhinaba Chowdhury and I'm a sophomore at IIEST Shibpur!",
+  githubUrl: "https://github.com/AbhiC7721",
+  linkedinUrl: "https://www.linkedin.com/in/abhichowdhury7721",
+  cvLink: "https://drive.google.com/file/d/1y2IzU5UlaBxR2Ik0JLzGJ3_y0axVd6aX/view?usp=sharing",
+};
